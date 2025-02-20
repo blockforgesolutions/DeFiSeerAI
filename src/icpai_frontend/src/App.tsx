3026ca92810e1da2b8abe1586b84765ee1541ea0
Yesarib <yesaribaris23@gmail.com>
2025-02-20 16:40:05 +0300

    feat: load provider

--- src/icpai_frontend/src/App.tsx
+++ src/icpai_frontend/src/App.tsx
@@ -2,28 +2,31 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { routes } from './routes';
 import MainLayout from './layouts/main';
 import DashboardLayout from './layouts/dashboard';
-import { AuthProvider } from './hooks/AuthProvider';
+import { AuthProvider } from './context/AuthProvider';
+import { LoadingProvider } from './context/loading-context';
 
 function App() {
 
 
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          {routes.map((routeGroup, index) => {
-            const Layout = routeGroup.layout === 'main' ? MainLayout : DashboardLayout;
+      <LoadingProvider>
+        <Router>
+          <Routes>
+            {routes.map((routeGroup, index) => {
+              const Layout = routeGroup.layout === 'main' ? MainLayout : DashboardLayout;
 
-            return routeGroup.pages.map((page, pageIndex) => (
-              <Route
-                key={pageIndex}
-                path={page.path}
-                element={<Layout>{page.element}</Layout>}
-              />
-            ));
-          })}
-        </Routes>
-      </Router>
+              return routeGroup.pages.map((page, pageIndex) => (
+                <Route
+                  key={pageIndex}
+                  path={page.path}
+                  element={<Layout>{page.element}</Layout>}
+                />
+              ));
+            })}
+          </Routes>
+        </Router>
+      </LoadingProvider>
     </AuthProvider>
   );
 }
--- src/icpai_frontend/src/layouts/dashboard.tsx
+++ src/icpai_frontend/src/layouts/dashboard.tsx
@@ -2,16 +2,21 @@ import React, { useEffect } from 'react';
 import Sidebar from '../components/dashboard/sidebar';
 import { DashboardNavbar } from '@/components/dashboard/navbar';
 import { icpai_backend } from '../../../declarations/icpai_backend';
+import { useLoading } from '@/context/loading-context';
+import GlobalLoading from '@/components/ui/loading';
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  const { setLoading } = useLoading();
 
   useEffect(() => {
     const fetchAndTrain = async () => {
+      setLoading(true);
       await icpai_backend.fetchAndTrain();
+      setLoading(false);
     };
 
     fetchAndTrain();
@@ -20,6 +25,7 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
 
   return (
     <div style={{ display: 'flex' }}>
+      <GlobalLoading />
       <aside className='lg:w-64'>
         <Sidebar />
       </aside>
